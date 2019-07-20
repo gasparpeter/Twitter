6283095396941bb6cec89e5ce1fce25458198641
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-20 15:40:01 +0300

    - monk

--- server/index.js
+++ server/index.js
@@ -1,6 +1,32 @@
 const express = require('express');
+const cors = require('cors');
+const monk = require('monk');
 
 const app = express();
+
+const db = monk('localhost/meower');
+
+[{
+    name: ..,
+    content: ..,
+    _id: ..
+}, {
+    name: ..,
+    content: ..,
+    _id: ..
+}, {
+    name: ..,
+    content: ..,
+    _id: ..
+}, {
+    name: ..,
+    content: ..,
+    _id: ..
+}]
+
+app.use(cors());
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.json({
@@ -8,8 +34,25 @@ app.get('/', (req, res) => {
     });
 });
 
+function isValidMew(mew) {
+    return mew.name && mew.name.toString().trim() !== '' &&
+        mew.content && mew.content.toString().trim() !== '';
+}
+
 app.post('/mews', (req, res) => {
-    console.log(req.body)
+    if (isValidMew(req, res)) {
+        //insert into db..
+        const mew = {
+            name: req.body.name.toString(),
+             content: req.body.name.toString()
+        };
+        console.log(mew);
+    }else {
+        res.status(422);
+        res.json({
+            message: 'Hey! Name and content are required!'
+        })
+    }
 });
 
 app.listen(5000, () => {

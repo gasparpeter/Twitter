bd57a05211e048319a2548db4f265a9348ab2b59
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-20 16:03:25 +0300

    - created mew

--- client/client.js
+++ client/client.js
@@ -2,7 +2,7 @@
 
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
-
+const API_URL = 'http://localhost:5000/mews';
 
 loadingElement.style.display = 'none';
 
@@ -22,14 +22,18 @@ form.addEventListener('submit', (event) => {
 
     form.style.display = 'none';
     loadingElement.style.display = '';
-    const API_URL = 'http://localhost:5000/mews';
+
     fetch(API_URL, {
         method: 'POST',
         body: JSON.stringify(mew),
         headers: {
             'content-type': 'application/json'
         }
-    });
-
+    }).then(response => response.json())
+      .then(createdMew => {
+          console.log(createdMew);
+          form.style.display = '';
+          loadingElement.style.display = 'none';
+  });
 });
 
--- server/index.js
+++ server/index.js
@@ -5,24 +5,8 @@ const monk = require('monk');
 const app = express();
 
 const db = monk('localhost/meower');
+const mews = db.get('mews');
 
-[{
-    name: ..,
-    content: ..,
-    _id: ..
-}, {
-    name: ..,
-    content: ..,
-    _id: ..
-}, {
-    name: ..,
-    content: ..,
-    _id: ..
-}, {
-    name: ..,
-    content: ..,
-    _id: ..
-}]
 
 app.use(cors());
 app.use(express.json());
@@ -41,12 +25,16 @@ function isValidMew(mew) {
 
 app.post('/mews', (req, res) => {
     if (isValidMew(req, res)) {
-        //insert into db..
         const mew = {
             name: req.body.name.toString(),
-             content: req.body.name.toString()
+            content: req.body.name.toString(),
+            created: new Date()
         };
-        console.log(mew);
+        mews
+            .insert(mew)
+            .then(createdMew => {
+                 res.json(createdMew);
+            });
     }else {
         res.status(422);
         res.json({

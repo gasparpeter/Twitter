d30b2db1297abd6f90516a50cc1ad61758b685bd
Peter Gaspar <petihappygaspar@gmail.com>
2019-07-22 14:52:00 +0300

    - twitter project end

--- client/client.js
+++ client/client.js
@@ -1,55 +1,94 @@
 
+console.log('Hello World!');
 
-const form = document.querySelector('form');
+const form = document.querySelector('form'); // grabbing an element on the page
+const errorElement = document.querySelector('.error-message');
 const loadingElement = document.querySelector('.loading');
-const API_URL = 'http://localhost:5000/mews';
 const mewsElement = document.querySelector('.mews');
+const loadMoreElement = document.querySelector('#loadMore');
+const API_URL = window.location.hostname === '127.0.0.1' ? 'http://localhost:5000/v2/mews' : 'https://meower-api.now.sh/v2/mews';
 
-loadingElement.style.display = '';
+let skip = 0;
+let limit = 5;
+let loading = false;
+let finished = false;
+
+errorElement.style.display = 'none';
+
+document.addEventListener('scroll', () => {
+    const rect = loadMoreElement.getBoundingClientRect();
+    if (rect.top < window.innerHeight && !loading && !finished) {
+        loadMore();
+    }
+});
 
 listAllMews();
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-
     const formData = new FormData(form);
     const name = formData.get('name');
     const content = formData.get('content');
 
+    if (name.trim() && content.trim()) {
+        errorElement.style.display = 'none';
+        form.style.display = 'none';
+        loadingElement.style.display = '';
 
-    const mew = {
-        name,
-        content
-    };
-    console.log(mew);
+        const mew = {
+            name,
+            content
+        };
 
-    form.style.display = 'none';
-    loadingElement.style.display = '';
-
-    fetch(API_URL, {
-        method: 'POST',
-        body: JSON.stringify(mew),
-        headers: {
-            'content-type': 'application/json'
-        }
-    }).then(response => response.json())
-      .then(createdMew => {
-          console.log(createdMew);
-          form.reset();
-          form.style.display = '';
-          listAllMews();
-          loadingElement.style.display = 'none';
-  });
+        fetch(API_URL, {
+            method: 'POST',
+            body: JSON.stringify(mew),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }).then(response => {
+            if (!response.ok) {
+                const contentType = response.headers.get('content-type');
+                if (contentType.includes('json')) {
+                    return response.json().then(error => Promise.reject(error.message));
+                } else {
+                    return response.text().then(message => Promise.reject(message));
+                }
+            }
+        }).then(() => {
+            form.reset();
+            setTimeout(() => {
+                form.style.display = '';
+            }, 30000);
+            listAllMews();
+        }).catch(errorMessage => {
+            form.style.display = '';
+            errorElement.textContent = errorMessage;
+            errorElement.style.display = '';
+            loadingElement.style.display = 'none';
+        });
+    } else {
+        errorElement.textContent = 'Name and content are required!';
+        errorElement.style.display = '';
+    }
 });
 
-function listAllMews() {
-    fetch(API_URL)
+function loadMore() {
+    skip += limit;
+    listAllMews(false);
+}
+
+function listAllMews(reset = true) {
+    loading = true;
+    if (reset) {
+        mewsElement.innerHTML = '';
+        skip = 0;
+        finished = false;
+    }
+    fetch(`${API_URL}?skip=${skip}&limit=${limit}`)
         .then(response => response.json())
-        .then(mews => {
-
-            mews.reverse();
-
-            mews.forEach(mew => {
+        .then(result => {
+            result.mews.forEach(mew => {
                 const div = document.createElement('div');
 
                 const header = document.createElement('h3');
@@ -66,7 +105,14 @@ function listAllMews() {
                 div.appendChild(date);
 
                 mewsElement.appendChild(div);
-
-            })
-        })
+            });
+            loadingElement.style.display = 'none';
+            if (!result.meta.has_more) {
+                loadMoreElement.style.visibility = 'hidden';
+                finished = true;
+            } else {
+                loadMoreElement.style.visibility = 'visible';
+            }
+            loading = false;
+        });
 }
